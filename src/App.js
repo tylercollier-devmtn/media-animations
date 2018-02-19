0f819b196448c73fee080a1bd5ad60a42dc956fe
Tyler Collier <tyler.collier+github@devmounta.in>
2018-02-19 11:03:16 -0700

    Media query

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <div className="logo">START BOOTSTRAP</div>
+          <div className="menu">MENU</div>
+          <div className="links">ABOUT DOWNLOAD CONTACT</div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
